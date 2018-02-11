ed9a83fcbfcccbf964ccfc0258bba479de71a1ca
Alex Martinez <alejandrorm@me.com>
2018-02-11 16:41:56 -0500

    commited ff_project

--- app/assets/javascripts/smart_hire.js
+++ app/assets/javascripts/smart_hire.js
@@ -1,7 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(function() {
-  let $jobbtn = $('#job_search');
-  $jobbtn.val("Job Title/Keyword").css({opacity: 0.3});
+ let $jobbtn = $('#job_search');
+ $jobbtn.val("Job Title/Keyword");
 
+
+
+ $jobbtn.click(function() {
+   $jobbtn.val("");
+ })
+
+ let $locbtn = $('#location');
+ $locbtn.val("City/State");
+
+ $locbtn.click(function() {
+   $locbtn.val("");
+ })
 });
