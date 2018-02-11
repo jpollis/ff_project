9c57f84119d25fd06fae38ec3e1e8e8a6bf472e7
Joe <jpollis.slp@gmail.com>
2018-02-11 13:21:06 -0500

    changes to search box.

--- app/assets/javascripts/smart_hire.js
+++ app/assets/javascripts/smart_hire.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function() {
+  let $jobbtn = $('#job_search');
+  $jobbtn.val("Job Title/Keyword").css({opacity: 0.3});
+
+});
