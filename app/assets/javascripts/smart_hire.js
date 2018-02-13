cb43f0037469e0ec16c63a8ac183b883057549df
Joe <jpollis.slp@gmail.com>
2018-02-12 19:31:54 -0500

    made signup/login smaller, centered text, added delete feature on results div.

--- app/assets/javascripts/smart_hire.js
+++ app/assets/javascripts/smart_hire.js
@@ -3,18 +3,26 @@
 
 $(function() {
  let $jobbtn = $('#job_search');
- $jobbtn.val("Job Title/Keyword");
+ $jobbtn.val("Job Title/Keyword").css({opacity: 0.5});
 
 
 
  $jobbtn.click(function() {
-   $jobbtn.val("");
+   $jobbtn.val("").css({opacity: 1.0});
  })
 
  let $locbtn = $('#location');
- $locbtn.val("City/State");
+ $locbtn.val("City/State").css({opacity: 0.5});
 
  $locbtn.click(function() {
-   $locbtn.val("");
+   $locbtn.val("").css({opacity: 1.0});
  })
+
+ let $result = $('.results');
+  let $deleteMsg = $('.delete');
+  $deleteMsg.on('click', function(){
+    let $job = $('.jobs');
+    $job.remove();
+  })
+
 });
