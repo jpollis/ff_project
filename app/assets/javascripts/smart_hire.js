5a7785ae114873f54fd941b42e8a1df9b664843c
Joe <jpollis.slp@gmail.com>
2018-02-21 09:24:00 -0500

    refactored a bit

--- app/assets/javascripts/smart_hire.js
+++ app/assets/javascripts/smart_hire.js
@@ -2,27 +2,6 @@
 // All this logic will automatically be available in application.js.
 
 $(function() {
- // let $jobbtn = $('#job_search');
- // $jobbtn.val("Job Title/Keyword");
-
- $jobbtn.click(function() {
-   $jobbtn.val("");
- })
-
- // $jobbtn.keypress(function() {
- //   $jobbtn.css({opacity: 1.0});
- // })
- // 
- // let $locbtn = $('#location');
- // $locbtn.val("City/State");
-
- $locbtn.on('click', function() {
-   $locbtn.val("").css({color: black});
- })
-
- // $locbtn.keypress(function() {
- //   $locbtn.css({opacity: 1.0});
- // })
 
  let $result = $('.results');
   let $deleteMsg = $('.delete');
