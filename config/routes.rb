96eec0f9400c1dfe792843932f64f677a271bf8d
Joe <jpollis.slp@gmail.com>
2018-03-03 09:51:30 -0500

    set root

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get '/' => "smart_hire#index", as: "main"
   post '/' => "smart_hire#index", as: "main_post"
+  root 'smart_hire#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
